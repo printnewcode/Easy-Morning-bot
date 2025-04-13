5ca00d0fbae10a0b1cd08ece02b53dc16f9537eb
--unset <zenit200803@mail.ru>
2025-04-13 20:47:25 +0300

    fix

--- bot/management/commands/delete_user.py
+++ bot/management/commands/delete_user.py
@@ -6,7 +6,7 @@ from bot import bot
 from bot.utils import access_time
 from bot.models import User
 from bot.keyboards import START_BUTTONS
-from Transition.settings import CHAT_ID
+from Transition.settings import CHAT_ID, OWNER_ID
 
 
 class Command(BaseCommand):
@@ -17,7 +17,10 @@ class Command(BaseCommand):
         for user in users:
             is_active = access_time.is_active(user)
             if not is_active:
-                bot.ban_chat_member(chat_id=CHAT_ID, user_id=int(user.telegram_id), revoke_messages=False)
+                try:
+                    bot.ban_chat_member(chat_id=CHAT_ID, user_id=int(user.telegram_id), revoke_messages=False)
+                except Exception as exc_:
+                    bot.send_message(chat_id=OWNER_ID, text=exc_)
                 bot.send_message(
                     chat_id=int(user.telegram_id),
                     text="Ваш доступ к нашей беседе закончился. Продлите его, оплатив подписку",
